676ff80e8ec417d8ee1aee7a79c3d77e9cee70ce
Brandon Santis <pcdude0@gmail.com>
2024-06-07 13:55:29 -0400

    change link

--- src/components/MainNavbarComponents/NavbarIcons.vue
+++ src/components/MainNavbarComponents/NavbarIcons.vue
@@ -27,7 +27,7 @@
     <div v-else-if="selectedIcon === 'gitSource'">
       <button
         class="border-2 rounded-md p-[0.35rem] svg_border"
-        onclick="window.open('https://github.com/Kuon-dev/kuon-portfolio/tree/main');"
+        onclick="window.open('https://github.com/AnimeFtw/brandon-portfolio');"
       >
         <img
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
@@ -50,57 +50,57 @@
 </template>
 
 <script>
-import { ref, watch } from 'vue';
-import MainThemeSelector from '../MainThemeSelectorComponents/MainThemeSelector.vue';
+  import { ref, watch } from 'vue';
+  import MainThemeSelector from '../MainThemeSelectorComponents/MainThemeSelector.vue';
 
-export default {
-	props: {
-		selectedIcon: {
-			type: String,
-			required: false,
-			default: null,
-		},
-	},
+  export default {
+    props: {
+      selectedIcon: {
+        type: String,
+        required: false,
+        default: null
+      }
+    },
 
-	components: {
-		MainThemeSelector,
-	},
+    components: {
+      MainThemeSelector
+    },
 
-	setup(props) {
-		const svgSource = './assets/theme.svg';
+    setup(props) {
+      const svgSource = './assets/theme.svg';
 
-		const isShowingThemeOverlay = ref(false);
+      const isShowingThemeOverlay = ref(false);
 
-		const toggleThemeOverlay = () => {
-			isShowingThemeOverlay.value = !isShowingThemeOverlay.value;
-		};
+      const toggleThemeOverlay = () => {
+        isShowingThemeOverlay.value = !isShowingThemeOverlay.value;
+      };
 
-		const closeThemeOverlay = () => {
-			isShowingThemeOverlay.value = false;
-		};
+      const closeThemeOverlay = () => {
+        isShowingThemeOverlay.value = false;
+      };
 
-		return {
-			svgSource,
-			toggleThemeOverlay,
-			isShowingThemeOverlay,
-			closeThemeOverlay,
-		};
-	},
-};
+      return {
+        svgSource,
+        toggleThemeOverlay,
+        isShowingThemeOverlay,
+        closeThemeOverlay
+      };
+    }
+  };
 </script>
 
 <style>
-.slide-fade-enter-active {
-  transition: all 0.3s ease-out;
-}
+  .slide-fade-enter-active {
+    transition: all 0.3s ease-out;
+  }
 
-.slide-fade-leave-active {
-  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
-}
+  .slide-fade-leave-active {
+    transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+  }
 
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-  transform: translateX(20px);
-  opacity: 0;
-}
+  .slide-fade-enter-from,
+  .slide-fade-leave-to {
+    transform: translateX(20px);
+    opacity: 0;
+  }
 </style>
